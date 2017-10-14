dfc735c0bda7eedbc2a572cc64c442e8e9b893ce
Richard L. Taylor Jr <rtaylor@rightincode.com>
2017-10-14 18:48:24 -0400

    Completed base functionality of tipcalc.

--- tipcalc_standard/ViewModels/MainPageViewModel.cs
+++ tipcalc_standard/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace tipcalc_standard.ViewModels
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
+        private double total;
+        private string totalTxt;
+
+        private double tip;
+
+        private double grandTotal;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string TotalTxt
+        {
+            get
+            {
+                return totalTxt;
+            }
+
+            set
+            {
+                totalTxt = value;
+
+                try
+                {
+                    string newValue = value;
+                    total = double.Parse(newValue);
+                }
+                catch (Exception)
+                {
+                    total = 0;
+                }
+                finally
+                {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TotalTxt"));
+                }
+            }
+        }
+
+        public string TipTxt
+        {
+            get { return tip.ToString(); }
+        }
+
+        public string GrandTotalTxt
+        {
+            get { return grandTotal.ToString(); }
+        }
+
+        public void CalcTip(double tipPercent)
+        {
+            if (tipPercent > 0)
+            {
+                tip = total * (tipPercent/100);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TipTxt"));
+                UpdateGrandTotal();
+            }            
+        }
+
+        private void UpdateGrandTotal()
+        {
+            grandTotal = total + tip;
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("GrandTotalTxt"));
+        }
     }
 }
--- tipcalc_standard/Views/MainPage.xaml.cs
+++ tipcalc_standard/Views/MainPage.xaml.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tipcalc_standard.ViewModels;
 using Xamarin.Forms;
 
 namespace tipcalc
 {
     public partial class MainPage : ContentPage
     {
+        private MainPageViewModel vm;
+
+        public MainPageViewModel VM
+        {
+            get { return vm; }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            vm = new MainPageViewModel();
+            BindingContext = VM;
+        }
+
+        public void onTipPercentageValueChanged(Object control, ValueChangedEventArgs args)
+        {
+            VM.CalcTip(args.NewValue);
         }
     }
 }
