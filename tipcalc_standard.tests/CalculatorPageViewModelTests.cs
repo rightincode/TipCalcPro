3c4feae4e77bfa44f922a566d3ed1242b50e6bab
Richard Taylor <rtaylor@rightincode.com>
2018-05-28 12:40:21 -0400

    Added unit test for CalculatorPageViewModel

---
+++ tipcalc_standard.tests/CalculatorPageViewModelTests.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using tipcalc_core.Models;
+using tipcalc_standard.ViewModels;
+
+namespace tipcalc_standard.tests
+{
+    [TestClass]
+    public class CalculatorPageViewModelTests
+    {
+        [TestMethod]
+        public void SetTotalText_ValidPositiveNumericalEntry_CalculatorContainsMatchingTotalDecimalValue()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TotalTxt = "55.34"
+            };
+
+            Assert.AreEqual(decimal.Parse(myCalculatorViewModel.TotalTxt), myCalculator.Total);
+
+        }
+
+        [TestMethod]
+        public void SetTotalText_ValidNegativeNumericalEntry_CalculatorContainsMatchingTotalDecimalValue()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TotalTxt = "-55.34"
+            };
+
+            Assert.AreEqual(decimal.Parse(myCalculatorViewModel.TotalTxt), myCalculator.Total);
+
+        }
+
+        [TestMethod]
+        public void SetTotalText_InValidEntry_CalculatorContainsZeroForTotal()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TotalTxt = "ABCD"
+            };
+
+            Assert.AreEqual(0, myCalculator.Total);
+
+        }
+
+        [TestMethod]
+        public void SetTipText_ValidPositiveNumericalEntry_CalculatorContainsMatchingTipDecimalValue()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TipTxt = "5.34"
+            };
+
+            Assert.AreEqual(decimal.Parse(myCalculatorViewModel.TipTxt), myCalculator.Tip);
+
+        }
+
+        [TestMethod]
+        public void SetTipText_ValidNegativeNumericalEntry_CalculatorContainsMatchingTipDecimalValue()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TipTxt = "-5.34"
+            };
+
+            Assert.AreEqual(decimal.Parse(myCalculatorViewModel.TipTxt), myCalculator.Tip);
+
+        }
+
+        [TestMethod]
+        public void SetTipText_InValidEntry_CalculatorContainsZeroForTip()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TipTxt = "ABCD"
+            };
+
+            Assert.AreEqual(0, myCalculator.Tip);
+
+        }
+
+        [TestMethod]
+        public void SetTipPercent_ValidPositiveNumericalEntry_CalculatorContainsMatchingTipDecimalValue()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TipPercent = 10
+            };
+
+            Assert.AreEqual(myCalculatorViewModel.TipPercent, myCalculator.TipPercent);
+        }
+
+        [TestMethod]
+        public void SetTipPercent_ValidNegativeNumericalEntry_CalculatorContainsMatchingTipDecimalValue()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TipPercent = -10
+            };
+
+            Assert.AreEqual(myCalculatorViewModel.TipPercent, myCalculator.TipPercent);
+        }
+
+        [TestMethod]
+        public void Calculate10PercentTipOn100Total_ValidInput_TipIs10Dollars_TotalIs110Dollars()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TotalTxt = "100",
+                TipPercent = 10
+            };
+                        
+            Assert.AreEqual("10.00", myCalculatorViewModel.TipTxt);
+            Assert.AreEqual(10, myCalculatorViewModel.TipPercent);
+            Assert.AreEqual("10", myCalculatorViewModel.TipPercentTxt);
+            Assert.AreEqual("110.00", myCalculatorViewModel.GrandTotalTxt);
+        }
+
+        [TestMethod]
+        public void Calculate18PercentTipOn35Total_ValidInput_TipIs6_30Dollars_TotalIs41_30Dollars()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TotalTxt = "35",
+                TipPercent = 18
+            };
+
+            Assert.AreEqual("6.30", myCalculatorViewModel.TipTxt);
+            Assert.AreEqual(18, myCalculatorViewModel.TipPercent);
+            Assert.AreEqual("18", myCalculatorViewModel.TipPercentTxt);
+            Assert.AreEqual("41.30", myCalculatorViewModel.GrandTotalTxt);
+        }
+
+        [TestMethod]
+        public void CalculateTipPercentageFor10DollarTipOn100Total_ValidInput_TipPercentageIs10_TotalIs110Dollars()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TotalTxt = "100",
+                TipTxt = "10"
+            };
+
+            Assert.AreEqual("10.00", myCalculatorViewModel.TipTxt);
+            Assert.AreEqual(10, myCalculatorViewModel.TipPercent);
+            Assert.AreEqual("10", myCalculatorViewModel.TipPercentTxt);
+            Assert.AreEqual("110.00", myCalculatorViewModel.GrandTotalTxt);
+        }
+
+        [TestMethod]
+        public void CalculateTipPercentFor6_30On35Total_ValidInput_TipPercentageIs18_TotalIs41_30Dollars()
+        {
+            var myCalculator = new TipCalculator();
+            var myCalculatorViewModel = new CalculatorPageViewModel(myCalculator)
+            {
+                TotalTxt = "35",
+                TipTxt = "6.30"
+            };
+
+            Assert.AreEqual("6.30", myCalculatorViewModel.TipTxt);
+            Assert.AreEqual(18, myCalculatorViewModel.TipPercent);
+            Assert.AreEqual("18", myCalculatorViewModel.TipPercentTxt);
+            Assert.AreEqual("41.30", myCalculatorViewModel.GrandTotalTxt);
+        }
+    }
+}
---
+++ tipcalc_standard.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tipcalc_standard.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tipcalc_standard.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8037fc37-b98a-4a76-8b19-4d1e0da59917")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
