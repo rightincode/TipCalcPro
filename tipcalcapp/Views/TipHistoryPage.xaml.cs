7ae2fc66f2b30a6508d927a33a403282cbc7ab74
Richard Taylor <rtaylor@rightincode.com>
2018-06-02 16:46:22 -0400

    Added view tip history

--- tipcalcapp/Views/TipHistoryPage.xaml.cs
+++ tipcalcapp/Views/TipHistoryPage.xaml.cs
@@ -1,14 +1,23 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using tipcalcapp.ViewModels;
+using tipcalc_data.Interfaces;
 
 namespace tipcalcapp.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class TipHistoryPage : ContentPage
 	{
-		public TipHistoryPage ()
+        private readonly ITipDatabase _tipDatabase = ((tipcalc.App)Application.Current).ServiceProvider.GetService<ITipDatabase>();
+
+        public TipHistoryPageViewModel VM { get; }
+
+        public TipHistoryPage ()
 		{
-			InitializeComponent ();
-		}
+			InitializeComponent();
+            VM = new TipHistoryPageViewModel(_tipDatabase);
+            BindingContext = VM;
+            VM.LoadTipHistory();
+        }
 	}
 }
